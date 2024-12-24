0aa7c9a66c603b5ddd2dbb935e3943d6794084f5
张大彪 <zwj.5.3@outlook.com>
2024-12-24 14:08:11 +0800

    20241224
    更新MIT许可证内容，增加资源地址，删除部分空格

--- README.md
+++ README.md
@@ -1,10 +1,24 @@
-张大彪丨zhdabiao 202401192213
+张大彪丨zhdabiao 202412241404
 -------------------------------------------------------------------
     声明:
     本网页源码来自互联网,如有侵权,请在GitHub联系我!
     希望如下信息能帮助到你:
     提供页面主体源代码来自https://www.suyanw.cn/
+    资源地址https://www.suyanw.cn/15555.html
     提供页面背景源代码来自https://jsrun.net/
+    资源地址http://jsrun.net/mIzKp
     同时感谢jsrun站长的鼓励,不然我这个缝合怪说不定就做不出来,谢谢站长
-    date:19/01/2024
+
+    最后更新日期2024年12月24日
+
+    Statement:
+    The source code of this page comes from the Internet. If there is any infringement, please contact me on GitHub!
+    I hope the following information can help you:
+    The source code of the main body of the page is provided from https://www.suyanw.cn/
+    Resource address https://www.suyanw.cn/15555.html
+    The source code of the background of the page is provided from https://jsrun.net/
+    Resource address http://jsrun.net/mIzKp
+    At the same time, I would like to thank the jsrun webmaster for his encouragement, otherwise I might not be able to do this stitching monster, thank you webmaster
+
+    Last updated on December 24, 2024
 
--- main.js
+++ main.js
@@ -1,3 +1,45 @@
+/*
+MIT License
+
+Copyright (c) 2017 Pavel Dobryakov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/*
+MIT 许可证
+
+Copyright (c) 2017 Pavel Dobryakov
+
+在此授予获得此软件和相关文档文件（以下简称“软件”）副本的任何人免费许可，
+以无限制方式处理软件，包括但不限于使用、复制、修改、合并、发布、分发、
+再许可和/或销售软件副本的权利，并允许向其提供软件的人员这样做，但须遵守
+以下条件：
+
+上述版权声明和本许可声明应包含在软件的所有副本或重要部分中。
+
+软件按“原样”提供，不作任何形式的保证，无论明示或暗示，包括但不限于适销
+性、特定用途的适用性和不侵权性的保证。在任何情况下，作者或版权持有者均
+不对因本软件或使用本软件或其他交易而引起的或与之相关的任何索赔、损害或
+其他责任负责，无论是合同行为、侵权行为还是其他行为。
+*/
+
 'use strict';
 
 const canvas = document.getElementsByTagName('canvas')[0];
@@ -1709,7 +1751,6 @@ function hashCode (s) {
     return hash;
 };
 
-
  //============================================================================
  //丨   声明:
  //丨  本网页源码来自互联网,如有侵权,请在GitHub联系我!
@@ -1717,7 +1758,5 @@ function hashCode (s) {
  //丨  提供页面主体源代码来自https://www.suyanw.cn/
  //丨  提供页面背景源代码来自https://jsrun.net/
  //丨  同时感谢jsrun站长的鼓励,不然我这个缝合怪说不定就做不出来,谢谢站长
- //丨   张大彪丨zhdabiao
- //丨   date:19/01/2024
  //丨   https://github.com/zhdabiao/zhdabiao.github.io
  //=============================================================================
